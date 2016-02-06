0e5a2c85c18fdb4a0b7b3e59c3fc50091b651668
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-02-06 11:59:24 -0800

    added tests that check for a defined/empty name property in feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,10 +44,17 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have a defined name and name is not empty', function() {
+            for(var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+            }
+         })
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -60,6 +67,8 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
